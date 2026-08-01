a3f9f4b307bab901cc8921f6bf4682810163515c
brameodoo <brame.odoo@gmail.com>
2026-08-01 12:03:10 -0600

    Update mrp_production.py

--- mrp_labels_wcenter/models/mrp_production.py
+++ mrp_labels_wcenter/models/mrp_production.py
@@ -6,26 +6,33 @@ class MrpProduction(models.Model):
 
     # Campos requeridos por vistas XML previas de JKKPack
     design_no = fields.Char(string="N° de Diseño")
-    sale_reference = fields.Char(string="Referencia de Venta / Pedido", compute='_compute_sale_order_info', store=False)
-    customer_code = fields.Char(string="Código de Cliente", compute='_compute_sale_order_info', store=False)
-    partner_id = fields.Many2one('res.partner', string="Cliente", compute='_compute_sale_order_info', store=False)
     
-    # Campos propios del flujo de empaque
-    sale_order_id = fields.Many2one('sale.order', string="Pedido de Venta", compute='_compute_sale_order_info', store=True)
-    customer_po_no = fields.Char(string="Order Cliente PO", compute='_compute_sale_order_info', store=True)
+    # Campos computados guardados en BD (stored)
+    sale_order_id = fields.Many2one('sale.order', string="Pedido de Venta", compute='_compute_sale_order_stored', store=True)
+    customer_po_no = fields.Char(string="Order Cliente PO", compute='_compute_sale_order_stored', store=True)
     
+    # Campos computados dinámicos (non-stored)
+    sale_reference = fields.Char(string="Referencia de Venta / Pedido", compute='_compute_sale_order_dynamic', store=False)
+    customer_code = fields.Char(string="Código de Cliente", compute='_compute_sale_order_dynamic', store=False)
+    partner_id = fields.Many2one('res.partner', string="Cliente", compute='_compute_sale_order_dynamic', store=False)
+    
+    # Relaciones del flujo de empaque
     label_ids = fields.One2many('mrp.production.label', 'production_id', string="Etiquetas Emitidas")
     pallet_ids = fields.One2many('mrp.production.pallet', 'production_id', string="Tarimas / Pallets")
 
     @api.depends('origin')
-    def _compute_sale_order_info(self):
+    def _compute_sale_order_stored(self):
         for mo in self:
             so = False
             if mo.origin:
                 so = self.env['sale.order'].search([('name', '=', mo.origin)], limit=1)
-            
             mo.sale_order_id = so.id if so else False
             mo.customer_po_no = so.client_order_ref if so else ''
+
+    @api.depends('sale_order_id', 'origin')
+    def _compute_sale_order_dynamic(self):
+        for mo in self:
+            so = mo.sale_order_id
             mo.sale_reference = so.name if so else (mo.origin or '')
             mo.partner_id = so.partner_id.id if (so and so.partner_id) else False
             mo.customer_code = so.partner_id.ref if (so and so.partner_id) else ''
