87458fd51a5ccfbac0e13182d15cfe90b64ba2f6
brameodoo <brame.odoo@gmail.com>
2026-08-01 22:14:16 -0600

    Update __manifest__.py

--- mrp_labels_wcenter/__manifest__.py
+++ mrp_labels_wcenter/__manifest__.py
@@ -9,16 +9,25 @@
     'depends': [
         'mrp',
         'barcodes',
-        'sale_management',  # <-- Módulo de Ventas confirmado
+        'sale_management',
     ],
     'data': [
+        # 1. Seguridad y Acceso a Modelos
         'security/ir.model.access.csv',
+        
+        # 2. Secuencias y Datos Iniciales
         'data/ir_sequence_data.xml',
+        
+        # 3. Vistas de Wizards / Asistentes (Deben cargarse ANTES de ser llamadas en mrp_production)
+        'wizard/mrp_production_label_wizard_views.xml',
+        'wizard/mrp_label_reprint_wizard_views.xml',
+        
+        # 4. Vistas Principales
         'views/mrp_workcenter_views.xml',
         'views/product_template_views.xml',
         'views/mrp_production_views.xml',
-        'wizard/mrp_production_label_wizard_views.xml',
-        'wizard/mrp_label_reprint_wizard_views.xml',
+        
+        # 5. Reportes e Impresiones ZPL / PDF
         'report/mrp_packaging_label_reports.xml',
         'report/report_roll_label_zpl.xml',
         'report/report_box_label_zpl.xml',
